1d01a70e4bc29635f58745acaa4bf30fd91bd078
Anna Lewis <anna.noel.lewis@gmail.com>
2018-05-27 09:56:51 -0600

    Import Appointments component into routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
+import Appointments from '@/components/Appointments';
 
 Vue.use(Router);
 
