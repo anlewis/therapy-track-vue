f7897477ce2686606a96e9caefbaf3c6b01d2c60
Anna Lewis <anna.noel.lewis@gmail.com>
2018-05-27 16:22:10 -0600

    Make appointments page pretty

--- src/components/Appointments.vue
+++ src/components/Appointments.vue
@@ -1,20 +1,29 @@
 <template>
   <div id="appointments">
-    <h2>Hello world</h2>
-    <div v-for="(appointment) in appointments" :key="appointment.id">
-      <p>My Appointment: {{appointment.summary}}</p>
-    </div>
+    <h1 class="title">Appointments</h1>
+      <div v-for="(appointment) in appointments" :key="appointment.id" class="appointment">
+        <h2><strong>{{appointment.summary}}</strong></h2>
+        <p>{{formatDate(appointment.start_time)}}</p>
+        <p>{{formatTime(appointment.start_time)}} - {{formatTime(appointment.end_time)}}</p>
+        <p>Location: {{appointment.location}}</p>
+        <p>Notes: {{appointment.description}}</p>
+      </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import moment from 'moment';
 
 export default {
   data() {
     return {
       appointments: [],
       summary: [],
+      location: [],
+      description: [],
+      start_time: [],
+      end_time: [],
       errors: [],
     };
   },
@@ -27,8 +36,31 @@ export default {
         this.error.push(e);
       });
   },
+  methods: {
+    formatDate(iso8601) {
+      const dateTime = new Date(iso8601)
+      return moment(dateTime).format("dddd, MMMM Do YYYY"); 
+    },
+    formatTime(iso8601) {
+      const dateTime = new Date(iso8601)
+      return moment(dateTime).format("h:mm a"); 
+    }
+  },
 };
 </script>
 
 <style>
+  body {
+    background-color: #DEE5E5;
+  }
+
+  .appointment {
+    background-color: #F9F8E8;
+    border: thick double #58808F;
+    margin: 5% 5%;
+  }
+
+  p {
+    margin: 5px;
+  }
 </style>
