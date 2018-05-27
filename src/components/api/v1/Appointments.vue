f9894cd7a9b0c5387022856351cb98242469ab81
Anna Lewis <anna.noel.lewis@gmail.com>
2018-05-27 09:51:06 -0600

    Add script and style to Appointments component [#157900573]

--- src/components/api/v1/Appointments.vue
+++ src/components/api/v1/Appointments.vue
@@ -1,7 +1,32 @@
 <template>
- <div id="appointments">
-  <div v-for="(appointment, index) in appointments" :key="appointment.id">
-   <p>My Appointment: {{appointment.summary}}</p>
+  <div id="appointments">
+    <div v-for="(appointment) in appointments" :key="appointment.id">
+      <p>My Appointment: {{appointment.summary}}</p>
+    </div>
   </div>
- </div>
 </template>
+
+<script>
+  import axios from 'axios'
+  export default {
+    data() {
+      return {
+        appointments: [],
+        summary: [],
+        errors: []
+      }
+    },
+    created() {
+      axios.get('localhost:3000/api/v1/appointments') 
+      .then(response => {
+        this.appointments = response.data
+      })
+      .catch(e => {
+        this.error.push(e)
+      })
+    },
+  }
+</script>
+
+<style>
+</style>
