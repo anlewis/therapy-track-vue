fc196100cd5e463f666b78a81bd1a1d4cdc6fc70
Anna Lewis <anna.noel.lewis@gmail.com>
2018-05-28 17:09:33 -0600

    Add basic root page

---
+++ src/components/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <h1>Welcome to Therapy Track!</h1>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import moment from 'moment';
+
+export default {
+  data() {
+    return {
+      data: []
+    }
+  },
+  created() {
+    axios.get('http://localhost:3000/')
+      .then((response) => {
+        this.data = response.data;
+      })
+      .catch((e) => {
+        this.error.push(e);
+      });
+  },
+  methods: {
+  },
+};
+</script>
+
+<style>
+  body {
+    background-color: #DEE5E5;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import Home from '@/components/Home';
 import Appointments from '@/components/Appointments';
 import Appointment from '@/components/Appointment';
 import AppointmentNew from '@/components/AppointmentNew';
@@ -13,8 +13,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      name: 'Home',
+      component: Home,
     },
     {
       path: '/appointments',
@@ -41,5 +41,10 @@ export default new Router({
       name: 'AppointmentDelete',
       component: AppointmentDelete,
     },
+    // {
+    //   path: '/appointments/:id/delete',
+    //   name: 'AppointmentDelete',
+    //   component: AppointmentDelete,
+    // },
   ],
 });
