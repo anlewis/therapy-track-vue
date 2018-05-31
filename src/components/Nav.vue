693b75e3960297f00621a3239fcd309b108fd645
Anna Lewis <anna.noel.lewis@gmail.com>
2018-05-30 19:31:42 -0600

    Add 'nav bar' to all pages [#157957821]

---
+++ src/components/Nav.vue
@@ -0,0 +1,17 @@
+<template>
+  <div> 
+    <h2>Nav Bar</h2> 
+    <nav> 
+      <router-link :to="{ name: 'Appointments' }">Appointments</router-link>
+    </nav> 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+};
+</script>
+
+<style>
+</style>
